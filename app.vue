7d3cb7f232a982172b9d88ca5814a9deac3be789
Olivier Odiet <olivier.odiet@gmail.com>
2024-11-25 02:27:14 +0100

    Intall: Tailwind + Flowbite + Modules: Icon, Image, Gfonts

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="font-Inter container">
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useFlowbite } from "~/composables/useFlowbite";
+
+// Initialize Flowbite when the DOM is ready
+onMounted(() => {
+  useFlowbite((flowbite: { initFlowbite: () => void }) => {
+    // Safe initialization of Flowbite
+    flowbite.initFlowbite();
+  });
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+  googleFonts: {
+    families: {
+      Inter: true,
+      display: "swap",
+    },
+  },
+});
