c173ee4dac1ce8db146340697d25ce1f59047615
Olivier Odiet <olivier.odiet@gmail.com>
2024-11-29 02:21:55 +0100

    Add primary tcolor to navbar

--- components/nav/Bar.vue
+++ components/nav/Bar.vue
@@ -25,7 +25,7 @@
 
 <script lang="ts" setup>
 interface Props {
-  variant?: 'default' | 'neutral' | 'base'
+  variant?: 'default' | 'neutral' | 'base' | 'primary'
 }
 
 const props = defineProps<Props>()
@@ -36,6 +36,8 @@ const variantClass = computed(() => {
       return 'bg-neutral text-neutral-content'
     case 'base':
       return 'bg-base-300'
+    case 'primary':
+      return 'bg-primary text-primary-content'
     default:
       return 'bg-base-100'
   }
